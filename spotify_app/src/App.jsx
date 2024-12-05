d447a73dd8c19c93a69f2b9f24caa1ad98256235
Jagjeet Dangar <jagjeet.dangar.cg@gmail.com>
2024-12-05 10:42:23 +0530

    made the sidebar of the spotify clone by react js

--- spotify_app/src/App.jsx
+++ spotify_app/src/App.jsx
@@ -1,31 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Spotify from './assets/Spotifylogo.png'
+import home from './assets/home.png'
+import search from './assets/search.png'
+import Yourlibrary from './assets/Yourlibrary.png'
+import Create1 from './assets/Create (1).png'
+import Liked1 from './assets/Liked (1).png'
+import yourepisodes from './assets/your episodes.png'
+import grommeticonsinstalloption from './assets/grommet-icons_install-option.png'
+import toparrownavigation from './assets/top arrow navigation.png'
+ 
 
 function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="sidebar">
+         <div className="image"><img className='super' src={Spotify} alt=""/></div>
+         <div className="sidebartext">
+            <div><img src={home} alt=""/></div>
+            <div><h3>Home</h3></div>
+            <div><img src={search} alt=""/></div>
+            <div><h3>Search</h3></div>
+            <div><img className='bat' src={Yourlibrary} alt=""/></div>
+            <div><h3>Your library</h3></div>
+         </div>
+         <div className="sidebartext2">
+            <div><img className='alagthi' src={Create1} alt=""/></div>
+            <div><h3 className="hehe">Create Playlist</h3></div>
+            <div><img className='alagthi' src={Liked1} alt=""/></div>
+            <div><h3 className="hehe">Liked Songs</h3></div>
+            <div><img className='alagthi1' src={yourepisodes} alt=""/></div>
+            <div><h3 className="hehe">Your episodes</h3></div>
+         </div>
+         <div className="sidebartext3">
+            <div><h3 className="hehe">FAV</h3></div>
+            <div><h3 className="hehe">Daily Mix 1</h3></div>
+            <div><h3 id='bgt' className="hehe">Discover Weekly</h3></div>
+            <div><h3 className="hehe">Malyalam</h3></div>
+            <div><h3 id='from' className="hehe">Dance/Electronics Mix</h3></div>
+            <div><h3 className="hehe">EDM Popular</h3></div>
+            <div className='adele'><img height="20px'" src={grommeticonsinstalloption} alt=""/><h3 id='itsme' className="hehe">Install App</h3></div>
+         </div>
+         <div id="image">
+            <img className='rolling' src={toparrownavigation} alt=""/>
+         </div>
+    </div>
     </>
   )
 }
